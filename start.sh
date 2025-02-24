af324781c652967f1cf490da5585084d5c83ed97
cj <charlesjcoulter@gmail.com>
2025-02-24 14:28:09 -0500

    3

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Start your Node.js applications using pm2
-pm2 start /var/www/html/proxy-server.js
-pm2 start /var/www/html/jelly-proxy.js
-pm2-runtime start /var/www/html/proxy-server.js /var/www/html/jelly-proxy.js
